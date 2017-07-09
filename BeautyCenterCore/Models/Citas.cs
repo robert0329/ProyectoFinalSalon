b130532bee07836f53727d35c4d3f1ac997dc286
yinet Jaquez <yinet.Jaquez@hotmail.com>
2017-07-08 20:53:20 -0400

    klk con klk

--- BeautyCenterCore/Models/Citas.cs
+++ BeautyCenterCore/Models/Citas.cs
@@ -11,9 +11,17 @@ namespace BeautyCenterCore.Models
         [Key]
         public int CitaId { get; set; }
         public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Necesitamos el nombre del cliente")]
         public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "Ingrese el Servicio")]
         public string Servicio { get; set; }
+
+        [Required(ErrorMessage = "Ingrese La cantidad de personas")]
         public int CantPersonas { get; set; }
+
+        [Required(ErrorMessage = "La fecha es Obligatorio")]
         public DateTime Fecha { get; set; }
     }
 }
