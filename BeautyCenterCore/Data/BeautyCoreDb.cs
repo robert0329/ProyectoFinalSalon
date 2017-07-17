c381bf01bda22f561956eca3cd741259d6bdf44e
yinet Jaquez <yinet.Jaquez@hotmail.com>
2017-07-17 11:02:55 -0400

    arreglando

--- BeautyCenterCore/Data/BeautyCoreDb.cs
+++ BeautyCenterCore/Data/BeautyCoreDb.cs
@@ -15,10 +15,10 @@ namespace BeautyCenterCore.Models
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=ROBERT\\SERVER;Initial Catalog=BeautyCenter;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-
+            optionsBuilder.UseSqlServer("Server=tcp:beautycenter.database.windows.net,1433;Initial Catalog=BeautyCoreDb;Persist Security Info=False;User ID=yinetjc;Password=C@sa9797;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            //Data Source=ROBERT\\SERVER;Initial Catalog=BeautyCenter;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
         }
-    
+
 
         public DbSet<BeautyCenterCore.Models.Citas> Citas { get; set; }
 
